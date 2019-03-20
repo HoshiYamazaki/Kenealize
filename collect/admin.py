a98d24d6fc8c28697325f9a38f1239516d4dc9b8
Hoshi Yamazaki <hoshiyamazaki01@gmail.com>
2019-03-20 21:02:35 +0100

    Added models for collecting data, configured settings, installed sorl thumbnail

---
+++ collect/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from collect.models import Execution, Device
+
+
+@admin.register(Execution)
+class ExecutionAdmin(admin.ModelAdmin):
+    list_display = ('time', 'amount', 'execution')
+    search_fields = ('execution',)
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'domain', 'script')
+    search_fields = ('ip',)
---
+++ collect/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Execution(models.Model):
+    time = models.DateTimeField('Time of script finished')
+    amount = models.IntegerField('Amount of devices found')
+    execution = models.FloatField('NMap execution time')
+
+    class Meta:
+        verbose_name = 'Data from KenCan execution'
+        verbose_name_plural = 'Data from KenCan execution'
+
+    def __str__(self):
+        return self.time
+
+class Device(models.Model):
+    script = models.ForeignKey(Execution, related_name='devices', on_delete=models.CASCADE)
+    ip = models.CharField('IPv4 Address', max_length=255)
+    domain = models.CharField('Domain', max_length=255)
+
+    class Meta:
+        verbose_name = 'Device'
+        verbose_name_plural = 'Devices'
+
+    def __str__(self):
+        return self.ip
+
