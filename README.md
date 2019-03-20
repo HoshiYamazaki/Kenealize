4377254c4ec76621f0538027ce42478064147f61
Hoshi Yamazaki <hoshiyamazaki01@gmail.com>
2019-03-20 20:12:50 +0100

    Move KenCan into class, add README and requirements

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kenealize
+Network scanner based on `nmap` software written in python and django.
+
+### Requirements
+* Django 2.1.7
+* Python 3.7
+* Nmap 7.70
--- kencan.py
+++ kencan.py
@@ -1,29 +1,32 @@
 import subprocess
 
-data, err = subprocess.Popen(['nmap', '-sP', '192.168.1.1/24'], stdout=subprocess.PIPE).communicate()
+class KenCan():
+    """KenCan scanner."""
+    def __init__(self):
+        self.devices_ips = []
+        self.data, self.err = subprocess.Popen(['nmap', '-sP', '192.168.1.1/24'], stdout=subprocess.PIPE).communicate()
 
-devices_ips = []
+    def scan_network(self):
+        for line in self.data.splitlines():
+            if line.startswith(b'Nmap scan report for'):
+                line_data = line.decode("utf-8").split()
+                domain = ''
+                if len(line_data) > 5:
+                    domain = line_data[4]
+                    ip = line_data[5].replace('(', '').replace(')', '')
+                ip = line_data[4]
+                self.devices_ips.append({
+                    'ip': ip,
+                    'domain': domain
+                })
+            if line.startswith(b'Nmap done: '):
+                line_data = line.decode("utf-8").split()
+                device_amount = line_data[5].replace('(', '')
+                time = line_data[10]
+        return {
+            'devices': device_amount,
+            'time': time
+        }
 
-for line in data.splitlines():
-    if line.startswith(b'Nmap scan report for'):
-        line_data = line.decode("utf-8").split()
-        domain = ''
-        if len(line_data) > 5:
-            domain = line_data[4]
-            ip = line_data[5].replace('(', '').replace(')', '')
-        ip = line_data[4]
-        devices_ips.append({
-            'ip': ip,
-            'domain': domain
-        })
-    if line.startswith(b'Nmap done: '):
-        line_data = line.decode("utf-8").split()
-        device_amount = line_data[5].replace('(', '')
-        time = line_data[10]
-
-print({
-    'devices': device_amount,
-    'time': time
-})
-
-print(devices_ips)
+    def get_devices_ips(self):
+        return self.devices_ips
